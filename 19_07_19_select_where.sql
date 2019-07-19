f08f2734b192f3b0eba54347ebfa6d66ed30d626
Chocobe <nice_10bird@naver.com>
2019-07-19 17:52:17 +0900

    <master> : Fixed README.md

--- 19_07_19_select_where.sql
+++ 19_07_19_select_where.sql
@@ -3,3 +3,52 @@
 SELECT EMPNO, ENAME, JOP, SAL,DEPNO
     FROM EMP
     WHERE JOP = 'MANAGER';
+    
+    
+-- EMP 테이블에서 입사일자가 1982년 1월 1일 이후에 입사한 사원의 정보를
+-- 사원번호, 성명, 담당업무, 급여, 입사일자, 부서번호 를 출력하기
+SELECT EMPNO, ENAME, JOP, SAL, HIREDATE, DEPNO
+    FROM EMP
+    WHERE HIREDATE > '1982-01-01';
+    
+
+-- EMP 테이블에서 급여가 1250에서 1500 사이의 정보를
+-- 성명, 담당업무, 급여, 부서번호를 출력하기
+SELECT ENAME, JOP, SAL, DEPNO
+    FROM EMP
+    WHERE SAL BETWEEN 1250 AND 1500;
+    
+SELECT ENAME, JOP, SAL, DEPNO
+    FROM EMP
+    WHERE SAL >= 1250 AND SAL <= 1500;
+    
+    
+-- EMP 테이블에서 사원번호가 7902, 7788, 7566인 사원의 정보를 
+-- 사원번호, 성명, 담당업무, 급여, 입사일자 를 출력하기
+SELECT EMPNO, ENAME, JOP, SAL, HIREDATE
+    FROM EMP
+    WHERE EMPNO IN(7902, 7788, 7566);
+    
+    
+-- EMP 테이블에서 입사일자가 82년도에 입사한 사원의 정보를
+-- 사원번호, 성명, 담당업무, 급여, 입사일자, 부서번호를 출력하기
+SELECT EMPNO, ENAME, JOP, SAL, HIREDATE, DEPNO
+    FROM EMP
+    WHERE HIREDATE LIKE '82%';
+    
+
+-- 저장한 날짜 형식 바꾸기
+ALTER SESSION SET NLS_DATE_FORMAT = 'DD-MM-YY';
+
+SELECT * FROM EMP;
+
+ALTER SESSION SET NLS_DATE_FORMAT = 'YY-MM-DD';
+
+SELECT * FROM EMP;
+
+
+-- EMP 테이블에서 보너스가 NULL인 사원의 정보를
+-- 사원번호, 성명, 담당업무, 급여, 입사일자, 부서번호 를 출력하기
+SELECT EMPNO, ENAME, JOP, SAL, COMM, DEPNO
+    FROM EMP
+    WHERE COMM IS NULL;
---
+++ practice.sql
@@ -0,0 +1,15 @@
+DELETE FROM EMP;
+SELECT * FROM EMP;
+DROP TABLE EMP;
+
+CREATE TABLE emp(
+    empno       NUMBER(4) NOT NULL,
+    ename       VARCHAR2(10),
+    job         VARCHAR2(9),
+    mgr         NUMBER(4),
+    hiredate    DATE,
+    sal         NUMBER(7, 2),
+    comm        NUMBER(7, 2),
+    deptno      NUMBER(2)
+);
+
