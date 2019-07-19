135eae1bd75eed06f6004c1d9e94c9dab87ed976
Chocobe <nice_10bird@naver.com>
2019-07-19 15:26:23 +0900

    <master> : Added Ex_files

--- 19.07.19.sql
+++ 19.07.19.sql
@@ -1,5 +1,6 @@
 -- 2019. 07. 19
 
+
 -- 테이블 생성하기
 CREATE TABLE emp (
     empno       NUMBER(4) NOT NULL,
@@ -17,7 +18,7 @@ CREATE TABLE emp (
 DROP TABLE emp;
 
 
--- 테이블 생성하기
+-- 테이블 생성하기 (직원 : emp)
 CREATE TABLE emp (
     empno       NUMBER(4) NOT NULL,
     ename       VARCHAR2(10),
@@ -43,8 +44,10 @@ INSERT INTO emp
     TO_DATE('1981-02-20', 'YYYY-MM-DD'),
     1600, 300, 30);
 
+
 -- 데이터 조회하기
 SELECT * FROM emp;
+
 
 -- 예제 사용
 INSERT INTO emp VALUES
@@ -85,11 +88,50 @@ INSERT INTO EMP VALUES
 
 SELECT * FROM emp;
 
---- 부서 테이블 생성하기
+
+-- 테이블 생성하기 (부서 : dept)
 CREATE TABLE dept
     (DEPTNO NUMBER(2), DNAME VARCHAR2(14), LOC VARCHAR(13));
+    
     
 -- 부서 테이블에 튜블 삽입하기
 INSERT INTO dept VALUES(10, 'ACCOUNTING', 'NEW YORK');
 INSERT INTO dept VALUES(20, 'RESEARCH', 'DALLAS');
 INSERT INTO dept VALUES(30, 'SALES', 'CHICAGO');
+INSERT INTO dept VALUES(40, 'OPERATIONS', 'BOSTON');
+
+SELECT * FROM dept;
+
+
+-- 삭제하기 (DNAME : MILLER)
+DELETE FROM dept WHERE DNAME = 'OPERATIONS';
+
+SELECT * FROM dept;
+
+INSERT INTO dept
+    VALUES(40, 'OPERATIONS', 'BOSTON');
+    
+SELECT * FROM dept;
+
+
+-- 테이블 생성하기 (호봉 : SALGRADE)
+CREATE TABLE SALGRADE(
+    GRADE   NUMBER,
+    LOSAL   NUMBER,
+    HISAL   NUMBER);
+ 
+
+-- 튜플 삽입하기
+INSERT INTO SALGRADE VALUES(1, 700, 1200);
+INSERT INTO SALGRADE VALUES(2, 1201, 1400);
+INSERT INTO SALGRADE VALUES(3, 1401, 2000);
+INSERT INTO SALGRADE VALUES(4, 2001, 3000);
+INSERT INTO SALGRADE VALUES(5, 3001, 9999);
+
+
+-- 테이블 생성하기 (수당 : BONUS)
+CREATE TABLE BONUS(
+    ENAME   VARCHAR2(10),   -- 사원명
+    JOB     VARCHAR2(9),    -- 업무
+    SAL     NUMBER,         -- 급여
+    COMM    NUMBER);        -- 수당
